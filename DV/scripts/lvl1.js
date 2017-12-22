d096fa81fc37b121d7e5b145f0262db0cc264580
Nicolas Brunner <nicolas.brunner@epfl.ch>
2017-12-22 15:05:03 +0100

    Revert "Compressed js"
    This reverts commit 1450273c2b136a9fcae2adcc41ce0f4a981cd7f9.

--- DV/scripts/lvl1.js
+++ DV/scripts/lvl1.js
@@ -1,1 +1,123 @@
-function createLvl1Graph(genreYear){var cur_year=genreYear;var data;d3.json("../data/lvl1.json",function(error,f){var temp=f.filter(function(d){return d.year===cur_year});data=temp[0].count;var text="";var width=400;var height=400;var thickness=40;var duration=750;var radius=Math.min(width,height)/2;function clickedLvl1(d){groupGenre=d.data.genre;scale=20;g.transition().duration(2000);path.transition().duration(900)var node2=document.createElement("div");node2.id="map";document.getElementById('visualization').appendChild(node2);createLvl2Graph(cur_year,groupGenre);document.getElementById('chart').style.zIndex=-1;document.getElementById('chart').style.opacity=0;addYearButton(groupGenre)}function resetLvl1(){active=d3.select(null);svg.transition().delay(1000).duration(1500).attr("opacity","1")}var svg=d3.select("#chart").append('svg').attr('class','pie').attr('width',width+100).attr('height',height+100).attr("opacity","0");var g=svg.append('g').attr('transform','translate('+((width+50)/2)+','+((height+50)/2)+')');var arc=d3.svg.arc().innerRadius(radius-thickness).outerRadius(radius);var pie=d3.layout.pie().value(function(d){return d.percentage}).sort(null);var path=g.selectAll('path').data(pie(data)).enter().append("g").on("click",function(d){clickedLvl1(d)}).on("mouseover",function(d){let g=d3.select(this).style("cursor","pointer").style("fill","black").append("g").attr("class","text-group");g.append("text").attr("class","name-text").text(`${d.data.genre}`).attr('text-anchor','middle').attr('dy','-0.7em').style('fill',colors[d.data.genre]);g.append("text").attr("class","value-text").text(function(d){val=parseFloat(Math.round(d.data.percentage*100*100)/100).toFixed(2);return`${val}%`}).attr('text-anchor','middle').attr('dy','.6em')}).on("mouseout",function(d){d3.select(this).style("cursor","none").style("fill",colors[d.data.genre]).select(".text-group").remove()}).append('path').attr('d',arc).attr('fill',(d,i)=>colors[d.data.genre]).on("mouseover",function(d){d3.select(this).style("cursor","pointer").style("opacity","0.6").style("transform","scale(1.03)")}).on("mouseout",function(d){d3.select(this).style("cursor","none").style("opacity","1").style("transform","scale(1)")}).each(function(d,i){this._current=i});g.append('text').attr('text-anchor','middle').attr('dy','.35em').text(text);resetLvl1()})}
+function createLvl1Graph(genreYear) {
+  var cur_year = genreYear;
+  var data;
+
+  d3.json("../data/lvl1.json", function (error, f) {
+    var temp = f.filter(function(d) {return d.year === cur_year; });
+    data = temp[0].count;
+
+    var text = "";
+
+    var width = 400;
+    var height = 400;
+    var thickness = 40;
+    var duration = 750;
+
+    var radius = Math.min(width, height) / 2;
+
+    function clickedLvl1(d){
+      groupGenre = d.data.genre;
+      scale = 20;
+      //center into the clicked node
+      g.transition()
+        .duration(2000);
+      //change the opacity of the link
+      path.transition()
+        .duration(900)
+      //create the next lvl
+      //setTimeout(document.getElementById('chart').remove(), 2100);
+      //change the opacity of the link
+      var node2 = document.createElement("div");
+      node2.id = "map";
+      document.getElementById('visualization').appendChild(node2);
+      createLvl2Graph(cur_year, groupGenre);
+      document.getElementById('chart').style.zIndex = -1;
+      document.getElementById('chart').style.opacity = 0;
+      addYearButton(groupGenre);
+    }
+
+    function resetLvl1() {
+      active = d3.select(null);
+       svg.transition()
+        .delay(1000)
+        .duration(1500)
+        .attr("opacity", "1");
+    }
+
+    var svg = d3.select("#chart")
+    .append('svg')
+    .attr('class', 'pie')
+    .attr('width', width+100)
+    .attr('height', height+100)
+    .attr("opacity", "0");
+
+    var g = svg.append('g')
+    .attr('transform', 'translate(' + ((width+50)/2) + ',' + ((height+50)/2) + ')');
+
+    var arc = d3.svg.arc()
+    .innerRadius(radius - thickness)
+    .outerRadius(radius);
+
+    var pie = d3.layout.pie()
+    .value(function(d) {return d.percentage; })
+    .sort(null);
+
+    var path = g.selectAll('path')
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .on("click",function(d) {clickedLvl1(d)})
+    .on("mouseover", function(d) {
+          let g = d3.select(this)
+            .style("cursor", "pointer")
+            .style("fill", "black")
+            .append("g")
+            .attr("class", "text-group");
+
+          g.append("text")
+            .attr("class", "name-text")
+            .text(`${d.data.genre}`)
+            .attr('text-anchor', 'middle')
+            .attr('dy', '-0.7em')
+            .style('fill', colors[d.data.genre]);
+
+          g.append("text")
+            .attr("class", "value-text")
+            .text(function(d){
+                val = parseFloat(Math.round(d.data.percentage*100 * 100) / 100).toFixed(2);
+                return `${val}%`;})
+            .attr('text-anchor', 'middle')
+            .attr('dy', '.6em');
+        })
+      .on("mouseout", function(d) {
+          d3.select(this)
+            .style("cursor", "none")
+            .style("fill", colors[d.data.genre])
+            .select(".text-group").remove();
+        })
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', (d,i) => colors[d.data.genre])
+      .on("mouseover", function(d) {
+          d3.select(this)
+            .style("cursor", "pointer")
+            .style("opacity", "0.6")
+            .style("transform", "scale(1.03)");
+        })
+      .on("mouseout", function(d) {
+          d3.select(this)
+            .style("cursor", "none")
+            .style("opacity", "1")
+            .style("transform", "scale(1)");
+        })
+      .each(function(d, i) { this._current = i; });
+
+
+    g.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.35em')
+      .text(text);
+
+    resetLvl1();
+  });
+}
--- DV/scripts/navtransit.js
+++ DV/scripts/navtransit.js
@@ -1,1 +1,76 @@
-var homeButton=document.createElement("span");var yearButton=document.createElement("span");var genreButton=document.createElement("span");var a,b,c;var cur_lvl;homeButton.id="homeButton";yearButton.id="yearButton";genreButton.id="genreButton";function addHomeButton(year){a=document.createTextNode("Home");b=document.createTextNode(year);homeButton.appendChild(a);yearButton.appendChild(b);cur_lvl=1;document.getElementById('navbar').appendChild(homeButton);document.getElementById('navbar').appendChild(yearButton);homeButton.addEventListener("mouseover",function(){homeButton.style.cursor="pointer"});homeButton.addEventListener("click",function(){backLvl0()})}function addYearButton(genre){c=document.createTextNode(genre);cur_lvl=2;genreButton.appendChild(c);document.getElementById('navbar').appendChild(genreButton);yearButton.addEventListener("mouseover",function(){yearButton.style.cursor="pointer"});yearButton.addEventListener("click",function(){backLvl1()})}function backLvl0(){var newCircle=document.createElement("div");newCircle.id="circle";newCircle.align="center";document.getElementById('link_legend').style.display='block';if(cur_lvl==1){document.getElementById('chart').remove();document.getElementById('circle').remove();homeButton.removeChild(a);yearButton.removeChild(b);document.getElementById('homeButton').remove();document.getElementById('yearButton').remove();document.getElementById('visualization').appendChild(newCircle);d3.json("../data/lvl0.json",drawGraph)}else if(cur_lvl==2){document.getElementById('chart').remove();document.getElementById('circle').remove();document.getElementById('map').remove();homeButton.removeChild(a);yearButton.removeChild(b);genreButton.removeChild(c);document.getElementById('homeButton').remove();document.getElementById('yearButton').remove();document.getElementById('genreButton').remove();document.getElementById('visualization').appendChild(newCircle);d3.json("../data/lvl0.json",drawGraph)}}function backLvl1(){cur_lvl=1;genreButton.removeChild(c);document.getElementById('genreButton').remove();document.getElementById('map').remove();document.getElementById('chart').style.zIndex=1;document.getElementById('chart').style.opacity=1;yearButton.removeEventListener("click",function(){backLvl1()});yearButton.addEventListener("mouseover",function(){yearButton.style.cursor="auto"})}
+var homeButton = document.createElement("span");
+var yearButton = document.createElement("span");
+var genreButton = document.createElement("span");
+var a, b, c;
+var cur_lvl;
+
+homeButton.id = "homeButton";
+yearButton.id = "yearButton";
+genreButton.id = "genreButton";
+
+function addHomeButton(year) {
+  a = document.createTextNode("Home");
+  b = document.createTextNode(year);
+  homeButton.appendChild(a);
+  yearButton.appendChild(b);
+  cur_lvl = 1;
+
+  document.getElementById('navbar').appendChild(homeButton);
+  document.getElementById('navbar').appendChild(yearButton);
+  homeButton.addEventListener("mouseover", function() {homeButton.style.cursor = "pointer"});
+  homeButton.addEventListener("click", function() {backLvl0()});
+}
+
+function addYearButton(genre) {
+  c = document.createTextNode(genre);
+  cur_lvl = 2;
+  genreButton.appendChild(c);
+
+  document.getElementById('navbar').appendChild(genreButton);
+  yearButton.addEventListener("mouseover", function(){yearButton.style.cursor = "pointer"});
+  yearButton.addEventListener("click", function() {backLvl1()});
+}
+
+function backLvl0() {
+  var newCircle = document.createElement("div");
+  newCircle.id = "circle";
+  newCircle.align = "center";
+  document.getElementById('link_legend').style.display = 'block';
+
+  if (cur_lvl == 1) {
+    document.getElementById('chart').remove();
+    document.getElementById('circle').remove();
+    homeButton.removeChild(a);
+    yearButton.removeChild(b);
+    document.getElementById('homeButton').remove();
+    document.getElementById('yearButton').remove();
+    document.getElementById('visualization').appendChild(newCircle);
+    d3.json("../data/lvl0.json", drawGraph);
+  } else if (cur_lvl == 2) {
+    //background color is genre
+    //document.getElementById('viz_container').style.background = '#E3E5E2';
+    document.getElementById('chart').remove();
+    document.getElementById('circle').remove();
+    document.getElementById('map').remove();
+    homeButton.removeChild(a);
+    yearButton.removeChild(b);
+    genreButton.removeChild(c);
+    document.getElementById('homeButton').remove();
+    document.getElementById('yearButton').remove();
+    document.getElementById('genreButton').remove();
+    document.getElementById('visualization').appendChild(newCircle);
+    d3.json("../data/lvl0.json", drawGraph);
+  }
+}
+
+function backLvl1() {
+  cur_lvl = 1;
+  //document.getElementById('viz_container').style.background= '#E3E5E2';
+  genreButton.removeChild(c);
+  document.getElementById('genreButton').remove();
+  document.getElementById('map').remove();
+  document.getElementById('chart').style.zIndex = 1;
+  document.getElementById('chart').style.opacity = 1;
+  yearButton.removeEventListener("click", function() {backLvl1()});
+  yearButton.addEventListener("mouseover", function(){yearButton.style.cursor = "auto"});
+}
